aeedd47d5fa78f57d928caae3be7edd861190f31
leusovd <leusovden@gmail.com>
2020-02-24 15:13:50 +0200

    add corrections of homework #1

--- homework/1/app.js
+++ homework/1/app.js
@@ -1,7 +1,11 @@
+require('./logger.js');
 const { getArgs, getExtEnv } = require('./helpers.js');
 const showFiles = require('./show-files.js');
 
-const { path, deep, colors } = getArgs();
+
+const { path, deep } = getArgs();
 const ext = getExtEnv();
 
-showFiles(path, deep, colors, ext);
+showFiles(path, deep, ext);
+
+
--- homework/1/show-files.js
+++ homework/1/show-files.js
@@ -2,67 +2,61 @@ const path = require('path');
 const fs = require('fs');
 const chalk = require('chalk');
 
-module.exports = function showFiles(relPath, deep = 0, colors = [], ext = []) {
-	if (relPath === void 0 || !relPath) {
-		throw new TypeError('Path is not defined.');
+const params = {
+	deep: null,
+	ext: null
+};
+
+function recursive(currentPath, level = 0) {
+	let dirent;
+
+	try {
+		dirent = fs.readdirSync(currentPath, {
+			withFileTypes: true
+		});
+	} catch (error) {
+		console.log(chalk.red(error));
+		return;
 	}
 
-	let c = 0; // color index
+	let i = 0;
+
+	// Directory name heading
+	const dirPrefix = !level ? '' : ''.padStart(level, '+');
+	console.log(chalk.bold(`${dirPrefix}Files from ${currentPath}:`));
+
+	while (true) {
+		if (!dirent.length || i === dirent.length) break;
+
+		// Check if entry is a file
+		// And which of extensions to show
+		// Then output it into the console
+		if (
+			dirent[i].isFile() &&
+			(ext.includes(path.extname(dirent[i].name)) || !params.ext.length)
+		) {
+			const filePrefix = dirPrefix.replace(/\+/g, '-');
+			console.log(filePrefix + dirent[i].name);
+		}
+
+		// Recursive view of inner directories
+		if (dirent[i].isDirectory() && ++level !== params.deep) {
+			recursive(currentPath + path.sep + dirent[i].name, level++);
+		}
+
+		i++;
+	}
+};
+
+module.exports = function showFiles(dirPath, deep, ext) {
+	dirPath =	typeof dirPath === 'string' &&
+			(path.isAbsolute(dirPath) ? dirPath : path.join(__dirname, dirPath)) ||	'./';
+	params.deep = typeof deep === 'number' && deep || 0;
+	params.ext = Array.isArray(ext) && ext || [];
 
 	console.log(chalk.bold('------------------------------------'));
 	console.log(chalk.bold('        RECURSIVE DIR VIEW          '));
 	console.log(chalk.bold('------------------------------------'));
 
-	const recursive = (relPath, level = 0) => {
-		const absPath = path.join(__dirname, relPath);
-		let dirent;
-
-		try {
-			dirent = fs.readdirSync(absPath, {
-				withFileTypes: true
-			});
-		} catch (error) {
-			console.log(chalk.red(error));
-			return;
-		}
-
-		let i = 0;
-
-		// Directory name heading
-		const dirPrefix = !level ? '' : ''.padStart(level, '+');
-		console.log(chalk.bold(`${dirPrefix}Files from ${relPath}:`));
-
-		while (true) {
-			if (!dirent.length || i === dirent.length) break;
-
-			// Check if entry is a file 
-			// And which of extensions to show
-			// Then output it into the console
-			if (
-				dirent[i].isFile() &&
-				(ext.includes(path.extname(dirent[i].name)) || !ext.length)
-			) {
-				// Colored or simple output
-				const output = colors.length ?
-					chalk.keyword(colors[c])(dirent[i].name) :
-					dirent[i].name;
-
-				const filePrefix = dirPrefix.replace(/\+/g, '-');
-				console.log(filePrefix + output);
-
-				if (colors.length) {
-					c = c === colors.length - 1 ? 0 : c + 1;
-				}
-			}
-
-			// Recursive view of inner directories
-			if (dirent[i].isDirectory() && ++level !== deep) {
-				recursive(relPath + '/' + dirent[i].name, level++);
-			}
-
-			i++;
-		}
-	};
-
-	recursive(relPath);
-};;
+	recursive(dirPath);
+};
